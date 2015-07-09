c6d62c6748d93beccb7e71954693a9c0383a9713
Luis Cua Catzin <luiscuacatzin@Luiss-Mac.local>
2015-07-08 20:41:44 -0700

    Finalizado

--- HonolululuArt/ViewController.swift
+++ HonolululuArt/ViewController.swift
@@ -13,11 +13,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    var artworks = [Artwork]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
         centerMapOnLocation(initialLocation)
+        
+        
+        loadInitialData()
+        mapView.addAnnotations(artworks)
+        mapView.delegate = self
+        
+        /*let artwork = Artwork(title: "King David Kalakaua",
+            locationName: "Waikiki Gateway Park",
+            discipline: "Sculpture",
+            coordinate: CLLocationCoordinate2D(latitude: 21.283921, longitude: -157.831661))
+        
+        mapView.addAnnotation(artwork)
+        */
         
         }
         // Do any additional setup after loading the view, typically from a nib.
@@ -36,6 +51,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 */
+    
+    func loadInitialData() {
+        // 1
+        let fileName = NSBundle.mainBundle().pathForResource("PublicArt", ofType: "json");
+        var readError : NSError?
+        var data: NSData = NSData(contentsOfFile: fileName!, options: NSDataReadingOptions(0),
+            error: &readError)!
+        
+        // 2
+        var error: NSError?
+        let jsonObject: AnyObject! = NSJSONSerialization.JSONObjectWithData(data,
+            options: NSJSONReadingOptions(0), error: &error)
+        
+        // 3
+        if let jsonObject = jsonObject as? [String: AnyObject] where error == nil,
+            // 4
+            let jsonData = JSONValue.fromObject(jsonObject)?["data"]?.array {
+                for artworkJSON in jsonData {
+                    if let artworkJSON = artworkJSON.array,
+                        // 5
+                        artwork = Artwork.fromJSON(artworkJSON) {
+                            artworks.append(artwork)
+                    }
+                }
+        }
+    }
+    
+    var locationManager = CLLocationManager()
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        checkLocationAuthorizationStatus()
+    }
+    
 
 }
 
